50a727aec1195f988bc86495abb1bbe78402195b
Felix Roberge <roberge.felix@gmail.com>
2018-09-04 22:56:23 -0400

    Try to return the list of product

--- src/main/java/com/thecat/productService/services/impl/ProductService.java
+++ src/main/java/com/thecat/productService/services/impl/ProductService.java
@@ -1,8 +1,15 @@
 package com.thecat.productService.services.impl;
 
 import com.thecat.productService.entities.Product;
+import org.json.simple.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,22 +42,80 @@ public class ProductService {
 	 * 
 	 * @throws IOException
 	 */
-	public List<Product> selectAllProduct() { // throws IOException {
+	public Product selectAllProduct() { // throws IOException {
 
-		List<Product> returnList = new ArrayList<>();
+		Product listProduct = null;
 
-		Product p = new Product();
-		p.setId("1");
-		p.setName("test product");
-		p.setCategory("category");
-		p.setSubCategory_1("subcategory 1");
-		p.setSubCategory_2("subcategory 2");
-		p.setPrice("100.00");
-		p.setCreateDate("2018-09-04");
+		try {
+			URL url = new URL("http://dbservice:8080/DatabaseService/api/db/products");
 
-		returnList.add( p );
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-Type", "application/json");
 
-		return returnList;
-		
+			OutputStream os = conn.getOutputStream();
+//			os.write(parseInput(emailAddress, password).getBytes());
+			os.flush();
+
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+
+			listProduct = parseListOutput(new BufferedReader(new InputStreamReader((conn.getInputStream()))));
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return listProduct;
+	}
+
+	/**
+	 * Create the response from what was return in the service.
+	 *
+	 * @param response {@link BufferedReader}
+	 * @return
+	 */
+	private Product parseListOutput(BufferedReader response) throws IOException {
+		try {
+			List<Product> listProduct = new ArrayList<>();
+//			StringBuilder sb = new StringBuilder();
+			String line;
+
+			while ((line = response.readLine()) != null) {
+//				sb.append( line );
+				System.out.println( "first line " + line + "\n" );
+//				JSONObject obj = new JSONObject( sb.toString() );
+
+				Product p = new Product();
+				p.setId("1");
+				p.setName("test product");
+				p.setCategory("category");
+				p.setSubCategory_1("subcategory 1");
+				p.setSubCategory_2("subcategory 2");
+				p.setPrice("100.00");
+			}
+
+//			JSONObject obj = new JSONObject(sb.toString());
+			Product p = new Product();
+			p.setId("1");
+			p.setName("test product");
+			p.setCategory("category");
+			p.setSubCategory_1("subcategory 1");
+			p.setSubCategory_2("subcategory 2");
+			p.setPrice("100.00");
+
+//			User user = new User();
+//			user.setUsername(obj.getString("username"));
+			return p;
+		} catch (IOException ioe) {
+			throw ioe;
+		}
 	}
 }
